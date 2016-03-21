2c561638eba19f9117ca96d11c31cab6395a7e14
Andrej Sajenko <Andrej.Sajenko@mni.thm.de>
2016-03-21 11:46:23 +0100

    add test for ThrowabnleHelper

---
+++ georocket-server-api/test/java/io/georocket/util/PathUtilsTest.java
@@ -0,0 +1,34 @@
+package io.georocket.util;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by andrej on 21/03/16.
+ */
+public class PathUtilsTest {
+
+  @org.junit.Test
+  public void testJoin() throws Exception {
+
+  }
+
+  @org.junit.Test
+  public void testNormalize() throws Exception {
+
+  }
+
+  @org.junit.Test
+  public void testIsAbsolute() throws Exception {
+
+  }
+
+  @org.junit.Test
+  public void testRemoveLeadingSlash() throws Exception {
+
+  }
+
+  @org.junit.Test
+  public void testAddTrailingSlash() throws Exception {
+
+  }
+}
---
+++ georocket-server/src/test/java/io/georocket/util/ThrowableHelperTest.java
@@ -0,0 +1,84 @@
+package io.georocket.util;
+
+import io.vertx.core.eventbus.ReplyException;
+import io.vertx.core.eventbus.ReplyFailure;
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.Assert.*;
+
+/**
+ * Test {@link ThrowableHelper}
+ * @author Andrej Sajenko
+ */
+public class ThrowableHelperTest {
+
+  @Test
+  public void testThrowableToCode_ReplyException() throws Exception {
+    int expectedCode = 505;
+
+    Throwable throwable = new ReplyException(ReplyFailure.NO_HANDLERS, expectedCode, "Message");
+
+    int statusCode = ThrowableHelper.throwableToCode(throwable);
+
+    assertEquals(expectedCode, statusCode);
+  }
+
+  @Test
+  public void testThrowableToCode_IllegalArgument() throws Exception {
+    int expectedCode = 400;
+
+    Throwable throwable = new IllegalArgumentException();
+
+    int statusCode = ThrowableHelper.throwableToCode(throwable);
+
+    assertEquals(expectedCode, statusCode);
+  }
+
+  @Test
+  public void testThrowableToCode_FileNotFound() throws Exception {
+    int expectedCode = 404;
+
+    Throwable throwable = new FileNotFoundException();
+
+    int statusCode = ThrowableHelper.throwableToCode(throwable);
+
+    assertEquals(expectedCode, statusCode);
+  }
+
+  @Test
+  public void testThrowableToCode_Throwable() throws Exception {
+    int expectedCode = 500;
+
+    Throwable throwable = new Throwable();
+
+    int statusCode = ThrowableHelper.throwableToCode(throwable);
+
+    assertEquals(expectedCode, statusCode);
+  }
+
+  @Test
+  public void testThrowableToMessage_Message() throws Exception {
+    String expectedMessage = "A Error happen!";
+    String defaultMessage = "Oops!";
+
+    Throwable throwable = new Throwable(expectedMessage);
+
+    String message = ThrowableHelper.throwableToMessage(throwable, defaultMessage);
+
+    assertEquals(expectedMessage, message);
+  }
+
+  @Test
+  public void testThrowableToMessage_Default() throws Exception {
+    String defaultMessage = "Oops!";
+
+    Throwable throwable = new Throwable();
+
+    String message = ThrowableHelper.throwableToMessage(throwable, defaultMessage);
+
+    assertEquals(defaultMessage, message);
+  }
+
+}
