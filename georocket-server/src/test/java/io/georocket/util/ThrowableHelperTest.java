5bba5f0d0950f47e7825e6c3db8e34657ea94737
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2016-03-21 14:39:28 +0100

    Clean up ThrowableHelperTest

--- georocket-server/src/test/java/io/georocket/util/ThrowableHelperTest.java
+++ georocket-server/src/test/java/io/georocket/util/ThrowableHelperTest.java
@@ -1,114 +1,95 @@
 package io.georocket.util;
 
-import io.vertx.core.eventbus.ReplyException;
-import io.vertx.core.eventbus.ReplyFailure;
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 import java.io.FileNotFoundException;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import io.vertx.core.eventbus.ReplyException;
+import io.vertx.core.eventbus.ReplyFailure;
 
 /**
  * Test {@link ThrowableHelper}
  * @author Andrej Sajenko
  */
 public class ThrowableHelperTest {
-
   /**
    * Test code of {@link ReplyException}
-   *
-   * @throws Exception
    */
   @Test
-  public void testThrowableToCode_ReplyException() throws Exception {
+  public void testThrowableToCodeReplyException() {
     int expectedCode = 505;
-
+    
     Throwable throwable = new ReplyException(ReplyFailure.NO_HANDLERS, expectedCode, "Message");
-
+    
     int statusCode = ThrowableHelper.throwableToCode(throwable);
-
     assertEquals(expectedCode, statusCode);
   }
 
   /**
    * Test code of {@link IllegalArgumentException}
-   *
-   * @throws Exception
    */
   @Test
-  public void testThrowableToCode_IllegalArgument() throws Exception {
+  public void testThrowableToCodeIllegalArgument() {
     int expectedCode = 400;
-
+    
     Throwable throwable = new IllegalArgumentException();
-
+    
     int statusCode = ThrowableHelper.throwableToCode(throwable);
-
     assertEquals(expectedCode, statusCode);
   }
 
   /**
    * Test code of {@link FileNotFoundException}
-   *
-   * @throws Exception
    */
   @Test
-  public void testThrowableToCode_FileNotFound() throws Exception {
+  public void testThrowableToCodeFileNotFound() {
     int expectedCode = 404;
-
+    
     Throwable throwable = new FileNotFoundException();
-
+    
     int statusCode = ThrowableHelper.throwableToCode(throwable);
-
     assertEquals(expectedCode, statusCode);
   }
 
   /**
    * Test code of unknown throwables.
-   *
-   * @throws Exception
    */
   @Test
-  public void testThrowableToCode_Throwable() throws Exception {
+  public void testThrowableToCodeThrowable() {
     int expectedCode = 500;
-
+    
     Throwable throwable = new Throwable();
-
+    
     int statusCode = ThrowableHelper.throwableToCode(throwable);
-
     assertEquals(expectedCode, statusCode);
   }
 
   /**
    * Test throwable message
-   *
-   * @throws Exception
    */
   @Test
-  public void testThrowableToMessage_Message() throws Exception {
+  public void testThrowableToMessage_Message() {
     String expectedMessage = "A Error happen!";
     String defaultMessage = "Oops!";
-
+    
     Throwable throwable = new Throwable(expectedMessage);
-
+    
     String message = ThrowableHelper.throwableToMessage(throwable, defaultMessage);
-
     assertEquals(expectedMessage, message);
   }
 
   /**
    * Test default message
-   *
-   * @throws Exception
    */
   @Test
-  public void testThrowableToMessage_Default() throws Exception {
+  public void testThrowableToMessageDefault() {
     String defaultMessage = "Oops!";
-
+    
     Throwable throwable = new Throwable();
-
+    
     String message = ThrowableHelper.throwableToMessage(throwable, defaultMessage);
-
     assertEquals(defaultMessage, message);
   }
-
 }
