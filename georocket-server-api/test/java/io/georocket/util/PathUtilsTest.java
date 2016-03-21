4d889a304191560c91cbe2248891220c68655b23
Andrej Sajenko <Andrej.Sajenko@mni.thm.de>
2016-03-21 11:46:57 +0100

    - add test dependency for junit in gradle - add PathUtils test cases

--- georocket-server-api/test/java/io/georocket/util/PathUtilsTest.java
+++ georocket-server-api/test/java/io/georocket/util/PathUtilsTest.java
@@ -1,34 +1,143 @@
 package io.georocket.util;
 
 import static org.junit.Assert.*;
-
+import static io.georocket.util.PathUtils.*;
 /**
- * Created by andrej on 21/03/16.
+ * Test {@link PathUtils}
+ * @author Andrej Sajenko
  */
 public class PathUtilsTest {
 
+  /**
+   * Test join with preceding and descending slashes
+   *
+   * @throws Exception
+   */
   @org.junit.Test
-  public void testJoin() throws Exception {
+  public void plainJoin() throws Exception {
+    assertEquals("abc", join("abc"));
 
+    assertEquals("aa/bb", join("aa", "bb"));
+
+    assertEquals("/aa/bb", join("/aa", "bb"));
+
+    assertEquals("/aa/bb", join("/aa", "/bb"));
+
+    assertEquals("/aa/bb/", join("/aa", "/bb/"));
   }
 
+  /**
+   * Test join with null and empty arguments
+   *
+   * @throws Exception
+   */
+  @org.junit.Test
+  public void emptyArgumentsJoin() throws Exception {
+    assertEquals("", join());
+
+    assertEquals("aa", join("aa", null));
+
+    assertEquals("aa/bb", join("aa", "", "bb"));
+  }
+
+  /**
+   * Test join normalization
+   *
+   * @throws Exception
+   */
+  @org.junit.Test
+  public void normalizeJoin() throws Exception {
+    assertEquals("bb", join("./aa", "../bb"));
+
+    assertEquals("aa/bb", join("./aa", "./bb"));
+  }
+  /**
+   * Test normalize on double slashes
+   *
+   * @throws Exception
+   */
+  @org.junit.Test
+  public void normalizeNormalizedPaths() throws Exception {
+    assertEquals("abc", normalize("abc"));
+
+    assertEquals("/abc/abc", normalize("/abc/abc"));
+  }
+
+
+  /**
+   * Test normalize on double slashes
+   *
+   * @throws Exception
+   */
   @org.junit.Test
   public void testNormalize() throws Exception {
+    assertEquals("/abc", normalize("//abc"));
 
+    assertEquals("/abc/abc/", normalize("//abc//abc//"));
+
+    assertEquals("/abc/", normalize("/abc/"));
   }
 
+  /**
+   * Test normalize with relative in out paths.
+   *
+   * @throws Exception
+   */
+  @org.junit.Test
+  public void relativeInOutNormalize() throws Exception {
+    assertEquals("abc", normalize("./abc"));
+
+    assertEquals("../abc", normalize("../abc"));
+
+    assertEquals("../../abc", normalize("../../abc"));
+
+    assertEquals("bb", normalize("./abc/../bb"));
+
+    assertEquals("", normalize("./abc/../"));
+  }
+
+  /**
+   * Test the absolute path check.
+   *
+   * @throws Exception
+   */
   @org.junit.Test
   public void testIsAbsolute() throws Exception {
+    assertTrue(isAbsolute("/absolute/path"));
 
+    assertFalse(isAbsolute("relative/path"));
+
+    assertFalse(isAbsolute(""));
   }
 
+  /**
+   * Remove leading slash
+   *
+   * @throws Exception
+   */
   @org.junit.Test
   public void testRemoveLeadingSlash() throws Exception {
+    assertEquals("abc", removeLeadingSlash("/abc"));
 
+    assertEquals("abc/abc", removeLeadingSlash("/abc/abc"));
+
+    String pathWithoutLeadingSlash = "abc/abc";
+    assertEquals(pathWithoutLeadingSlash, removeLeadingSlash(pathWithoutLeadingSlash));
   }
 
+  /**
+   * Add trailing slash
+   *
+   * @throws Exception
+   */
   @org.junit.Test
   public void testAddTrailingSlash() throws Exception {
+    assertEquals("abc/", addTrailingSlash("abc"));
 
+    assertEquals("/abc/abc/", addTrailingSlash("/abc/abc"));
+
+    assertEquals("/abc/abc/", addTrailingSlash("/abc/abc/"));
+
+    assertEquals("/", addTrailingSlash(""));
   }
 }
