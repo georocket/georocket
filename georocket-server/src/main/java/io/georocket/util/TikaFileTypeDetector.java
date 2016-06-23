540cf9541ccd7d50e3e8ef7c596679ab841b2c43
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2016-06-23 10:54:12 +0200

    Improve formatting

--- georocket-server/src/main/java/io/georocket/util/TikaFileTypeDetector.java
+++ georocket-server/src/main/java/io/georocket/util/TikaFileTypeDetector.java
@@ -7,16 +7,13 @@ import java.nio.file.Path;
 import java.nio.file.spi.FileTypeDetector;
 
 /**
- * <p>File type detector that use the apache tika detector.</p>
- *
- * <p>This type detector use the magic number and file extension to guess the right content type.</p>
- *
+ * <p>File type detector that use the Apache Tika detector.</p>
+ * <p>This type detector uses the magic number and file extension to guess
+ * the right content type.</p>
  * <p>See {@link Tika#detect(Path)}</p>
- *
  * @author Andrej Sajenko
  */
 public class TikaFileTypeDetector extends FileTypeDetector {
-
   private Tika tika = new Tika();
 
   @Override
