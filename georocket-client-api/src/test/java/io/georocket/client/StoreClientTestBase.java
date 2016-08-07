aa7e76de0de9b9203210cf8be41cdce1aadda713
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2016-08-07 19:52:48 +0200

    Update WireMock in client-api

--- georocket-client-api/src/test/java/io/georocket/client/StoreClientTestBase.java
+++ georocket-client-api/src/test/java/io/georocket/client/StoreClientTestBase.java
@@ -1,8 +1,10 @@
 package io.georocket.client;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.configureFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.verify;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -13,17 +15,11 @@ import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.RunTestOnContext;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-
-
 /**
  * Base class for unit tests that test the {@link StoreClient}
  * @author Michel Kraemer
  */
 public abstract class StoreClientTestBase {
-  private static final int PORT = findPort();
-  
   /**
    * Run the test on a Vert.x test context
    */
@@ -34,7 +30,7 @@ public abstract class StoreClientTestBase {
    * Run a mock HTTP server
    */
   @Rule
-  public WireMockRule wireMockRule = new WireMockRule(PORT);
+  public WireMockRule wireMockRule = new WireMockRule(options().dynamicPort());
   
   /**
    * The client under test
@@ -47,7 +43,8 @@ public abstract class StoreClientTestBase {
    */
   @Before
   public void setUp() throws Exception {
-    client = new GeoRocketClient("localhost", PORT, rule.vertx());
+    configureFor("localhost", wireMockRule.port());
+    client = new GeoRocketClient("localhost", wireMockRule.port(), rule.vertx());
   }
   
   /**
@@ -60,28 +57,6 @@ public abstract class StoreClientTestBase {
       verify(getRequestedFor(urlEqualTo(url)));
     } catch (VerificationException e) {
       context.fail(e);
-    }
-  }
-
-  /**
-   * Find a free socket port.
-   * @return the number of the free port
-   */
-  private static int findPort() {
-    ServerSocket socket = null;
-    try {
-      socket = new ServerSocket(0);
-      return socket.getLocalPort();
-    } catch (IOException e) {
-      throw new RuntimeException("Could not find a free port for the test");
-    } finally {
-      if (socket != null) {
-        try {
-          socket.close();
-        } catch (IOException e) {
-          // ignored
-        }
-      }
     }
   }
 }
