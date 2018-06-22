750faa58fe4e25e6c5a0972a0ad11d229d581dcf
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2018-06-22 15:00:06 +0200

    Allow users to search for gmlId specifically

--- georocket-server/src/main/java/io/georocket/index/xml/GmlIdIndexerFactory.java
+++ georocket-server/src/main/java/io/georocket/index/xml/GmlIdIndexerFactory.java
@@ -5,6 +5,10 @@ import java.util.Map;
 import com.google.common.collect.ImmutableMap;
 
 import io.georocket.query.ElasticsearchQueryHelper;
+import io.georocket.query.KeyValueQueryPart;
+import io.georocket.query.KeyValueQueryPart.ComparisonOperator;
+import io.georocket.query.QueryPart;
+import io.georocket.query.StringQueryPart;
 import io.vertx.core.json.JsonObject;
 
 /**
@@ -24,13 +28,40 @@ public class GmlIdIndexerFactory implements XMLIndexerFactory {
     )));
   }
 
-  @Override
-  public MatchPriority getQueryPriority(String search) {
-    return MatchPriority.SHOULD;
+  /**
+   * Test if the given key-value query part refers to a gmlId and if it uses
+   * the EQ operator (e.g. EQ(gmlId myId) or EQ(gml:id myId))
+   * @param kvqp the key-value query part to check
+   * @return true if it refers to a gmlId, false otherwise
+   */
+  private static boolean isGmlIdEQ(KeyValueQueryPart kvqp) {
+    String key = kvqp.getKey();
+    ComparisonOperator comp = kvqp.getComparisonOperator();
+    return (comp == ComparisonOperator.EQ && ("gmlId".equals(key) || "gml:id".equals(key)));
   }
 
   @Override
-  public JsonObject compileQuery(String search) {
-    return ElasticsearchQueryHelper.termQuery("gmlIds", search);
+  public MatchPriority getQueryPriority(QueryPart queryPart) {
+    if (queryPart instanceof StringQueryPart) {
+      return MatchPriority.SHOULD;
+    }
+    if (queryPart instanceof KeyValueQueryPart && isGmlIdEQ((KeyValueQueryPart)queryPart)) {
+      return MatchPriority.SHOULD;
+    }
+    return MatchPriority.NONE;
+  }
+
+  @Override
+  public JsonObject compileQuery(QueryPart queryPart) {
+    if (queryPart instanceof StringQueryPart) {
+      String search = ((StringQueryPart)queryPart).getSearchString();
+      return ElasticsearchQueryHelper.termQuery("gmlIds", search);
+    } else if (queryPart instanceof KeyValueQueryPart) {
+      KeyValueQueryPart kvqp = (KeyValueQueryPart)queryPart;
+      if (isGmlIdEQ(kvqp)) {
+        return ElasticsearchQueryHelper.termQuery("gmlIds", kvqp.getValue());
+      }
+    }
+    return null;
   }
 }
