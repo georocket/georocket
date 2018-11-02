31e81b7a2bf0602df1f9e5629fe191141439636e
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2018-11-02 11:50:23 +0100

    Fix checkstyle error

--- georocket-server/src/main/java/io/georocket/http/StoreEndpoint.java
+++ georocket-server/src/main/java/io/georocket/http/StoreEndpoint.java
@@ -50,7 +50,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 import java.util.regex.Pattern;
 
 import static io.georocket.http.Endpoint.fail;
