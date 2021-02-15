19eb893a532af204f5dc7199b44826edc480b3ff
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2021-02-15 11:37:57 +0100

    Fix construction of store classes with optional constructor parameters

--- georocket-server/src/main/kotlin/io/georocket/storage/StoreFactory.kt
+++ georocket-server/src/main/kotlin/io/georocket/storage/StoreFactory.kt
@@ -3,6 +3,7 @@ package io.georocket.storage
 import io.georocket.constants.ConfigConstants
 import io.georocket.storage.file.FileStore
 import io.vertx.core.Vertx
+import kotlin.reflect.full.starProjectedType
 
 /**
  * A factory for stores
@@ -16,7 +17,12 @@ object StoreFactory {
     val config = vertx.orCreateContext.config()
     val cls = config.getString(ConfigConstants.STORAGE_CLASS, FileStore::class.java.name)
     return try {
-      Class.forName(cls).getConstructor(Vertx::class.java).newInstance(vertx) as Store
+      val kc = Class.forName(cls).kotlin
+      val constructor = kc.constructors.find { c ->
+        c.parameters.isNotEmpty() && c.parameters.first().type == Vertx::class.starProjectedType
+      } ?: throw RuntimeException("Storage class has no constructor with " +
+          "Vert.x object as its first parameter")
+      constructor.callBy(mapOf(constructor.parameters.first() to vertx)) as Store
     } catch (e: ReflectiveOperationException) {
       throw RuntimeException("Could not create chunk store", e)
     }
