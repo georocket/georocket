081407cb559b411c74854cfc4e9e64560a1fd3f1
Michel Kraemer <michel.kraemer@igd.fraunhofer.de>
2022-01-07 17:17:47 +0100

    Add more XML integration tests

--- integration-tests2/tests/_xml.js
+++ integration-tests2/tests/_xml.js
@@ -29,6 +29,75 @@ function xml(ctx) {
 
       expect(actual).toEqual(expected)
     })
+
+    it("returns one building by string search", async () => {
+      let res = await ctx.request.get("/store/?search=ID_147_D")
+      expect(res.status).toBe(200)
+
+      let parser = new XMLParser()
+      let data = parser.parse(res.data)
+
+      expect(Array.isArray(data.CityModel.cityObjectMember)).toBe(false)
+      expect(data.CityModel.cityObjectMember["bldg:Building"]
+        ["gen:stringAttribute"]["gen:value"]).toEqual("TestBuilding1")
+    })
+
+    it("returns other building by string search", async () => {
+      let res = await ctx.request.get("/store/?search=TestBuilding2")
+      expect(res.status).toBe(200)
+
+      let parser = new XMLParser()
+      let data = parser.parse(res.data)
+
+      expect(Array.isArray(data.CityModel.cityObjectMember)).toBe(false)
+      expect(data.CityModel.cityObjectMember["bldg:Building"]
+        ["gen:stringAttribute"]["gen:value"]).toEqual("TestBuilding2")
+    })
+
+    it("returns other building by EQ", async () => {
+      let res = await ctx.request.get("/store/?search=EQ(TestBuildingName TestBuilding2)")
+      expect(res.status).toBe(200)
+
+      let parser = new XMLParser()
+      let data = parser.parse(res.data)
+
+      expect(Array.isArray(data.CityModel.cityObjectMember)).toBe(false)
+      expect(data.CityModel.cityObjectMember["bldg:Building"]
+        ["gen:stringAttribute"]["gen:value"]).toEqual("TestBuilding2")
+    })
+
+    it("returns buildings by gml:id", async () => {
+      let res = await ctx.request.get("/store/?search=EQ(gmlId ID_147_D)")
+      expect(res.status).toBe(200)
+
+      let parser = new XMLParser()
+      let data = parser.parse(res.data)
+
+      expect(Array.isArray(data.CityModel.cityObjectMember)).toBe(false)
+      expect(data.CityModel.cityObjectMember["bldg:Building"]
+        ["gen:stringAttribute"]["gen:value"]).toEqual("TestBuilding1")
+
+      res = await ctx.request.get("/store/?search=EQ(gmlId ID_146_D)")
+      expect(res.status).toBe(200)
+
+      data = parser.parse(res.data)
+
+      expect(Array.isArray(data.CityModel.cityObjectMember)).toBe(false)
+      expect(data.CityModel.cityObjectMember["bldg:Building"]
+        ["gen:stringAttribute"]["gen:value"]).toEqual("TestBuilding2")
+    })
+
+    it("returns one building by bounding box", async () => {
+      let res = await ctx.request.get("/store/?search=13.04709829608855,52.33016111449572,13.047127397967273,52.330179231017645")
+      expect(res.status).toBe(200)
+
+      let parser = new XMLParser()
+      let data = parser.parse(res.data)
+
+      expect(Array.isArray(data.CityModel.cityObjectMember)).toBe(false)
+      expect(data.CityModel.cityObjectMember["bldg:Building"]
+        ["gen:stringAttribute"]["gen:value"]).toEqual("TestBuilding1")
+    })
   })
 }
 
