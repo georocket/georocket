76f4478540812805e3068d500479fb46a561ddb0
Tobias Dorra <public.tobias@outlook.com>
2022-02-16 16:17:19 +0100

    Use Jackson for XML serialisation in ogcapifeatures

--- src/main/kotlin/io/georocket/ogcapifeatures/views/Views.kt
+++ src/main/kotlin/io/georocket/ogcapifeatures/views/Views.kt
@@ -1,25 +1,42 @@
 package io.georocket.ogcapifeatures.views
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty
+import io.georocket.ogcapifeatures.views.xml.XML_NAMESPACE_ATOM
+import io.georocket.ogcapifeatures.views.xml.XML_NAMESPACE_CORE
 import io.vertx.core.http.HttpServerResponse
 
 interface Views {
 
   data class Link(
+    @JacksonXmlProperty(isAttribute = true)
     val href: String,
+
+    @JacksonXmlProperty(isAttribute = true)
     val rel: String? = null,
+
+    @JacksonXmlProperty(isAttribute = true)
     val type: String? = null,
+
+    @JacksonXmlProperty(isAttribute = true)
     val title: String? = null
   )
 
   data class Collection(
+    @JacksonXmlProperty(localName = "Id", namespace = XML_NAMESPACE_CORE)
     val id: String,
-    val title: String? = null,
+
+    @JacksonXmlProperty(localName = "Title", namespace = XML_NAMESPACE_CORE)
+    val title: String,
+
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @JacksonXmlProperty(localName = "Link", namespace = XML_NAMESPACE_ATOM)
     val links: List<Link>,
   )
 
   fun landingPage(response: HttpServerResponse, links: List<Link>)
 
-  fun conformances(response: HttpServerResponse, conformsTo: List<String>)
+  fun conformance(response: HttpServerResponse, conformsTo: List<String>)
 
   fun collections(response: HttpServerResponse, links: List<Link>, collections: List<Collection>)
 
