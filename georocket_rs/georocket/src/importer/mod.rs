8b763603d389d3bfe3c88c09d4cf277301d930f5
Lion~ Bernhard <Bernhard.l.thomas@protonmail.com>
2023-11-29 11:11:01 +0100

    Add basic implementation for importer

--- georocket_rs/georocket/src/importer/mod.rs
+++ georocket_rs/georocket/src/importer/mod.rs
@@ -1,21 +1,44 @@
-use tokio::io::AsyncRead;
-
+use crate::indexer::MainIndexer;
 use crate::input::Splitter;
 use crate::output::Store;
 
 pub struct GeoDataImporter {
-    splitter: Splitter,
-    store: Store,
+    splitter: Box<dyn Splitter + Send>,
+    store: Box<dyn Store + Send>,
+    indexer: MainIndexer,
 }
 
 impl GeoDataImporter {
-    pub fn new(splitter: Splitter, store: Store) -> Self {
-        Self { splitter, store }
+    pub fn new(
+        splitter: Box<dyn Splitter + Send>,
+        store: Box<dyn Store + Send>,
+        indexer: MainIndexer,
+    ) -> Self {
+        Self {
+            splitter,
+            store,
+            indexer,
+        }
     }
 
-    pub async fn run(self) -> anyhow::Result<()> {
-        let splitter_handle = tokio::spawn(self.splitter.run());
-        let store_handle = tokio::spawn(self.store.run());
-        Ok(())
+    pub async fn run(mut self) {
+        let mut splitter_handle = tokio::spawn(async move { self.splitter.run().await });
+        let mut store_handle = tokio::spawn(async move { self.store.run().await });
+        let mut index_handle = tokio::spawn(self.indexer.run());
+
+        loop {
+            tokio::select! {
+                splitt_res = &mut splitter_handle, if !splitter_handle.is_finished() => {
+
+                }
+                store_res = &mut store_handle, if !store_handle.is_finished() => {
+
+                }
+                index_res = &mut index_handle, if !index_handle.is_finished() => {
+
+                }
+                else => break,
+            }
+        }
     }
 }
