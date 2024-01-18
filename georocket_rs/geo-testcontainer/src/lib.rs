0206ab18e8fbd0221e62d1fb682355fdb7144d33
Lion~ Bernhard <Bernhard.l.thomas@protonmail.com>
2024-01-18 12:48:50 +0100

    Add postgis module to the geo-testcontainers library

--- georocket_rs/geo-testcontainer/src/lib.rs
+++ georocket_rs/geo-testcontainer/src/lib.rs
@@ -1,14 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+#[cfg(feature = "postgis")]
+#[cfg_attr(docsrs, doc(cfg(feature = "postgis")))]
+pub mod postgis;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub use testcontainers;
