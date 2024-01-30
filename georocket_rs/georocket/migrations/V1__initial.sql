386de44609718d22e859fa7731b7fad741a882ee
Lion~ Bernhard <Bernhard.l.Thomas@protonmail.com>
2024-01-30 16:59:16 +0100

    Add migration directory and file

---
+++ georocket_rs/georocket/migrations/V1__initial.sql
@@ -0,0 +1,17 @@
+CREATE SCHEMA IF NOT EXISTS georocket;
+
+CREATE TABLE georocket.feature (
+    id UUID,
+    raw_feature TEXT
+);
+
+CREATE TABLE georocket.bounding_box (
+    id UUID,
+    bounding_box GEOMETRY
+);
+
+CREATE TABLE georocket.property (
+    id UUID,
+    key TEXT,
+    VALUE TEXT
+);
