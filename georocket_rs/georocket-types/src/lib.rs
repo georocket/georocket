294cb53cf3a75cdb3daa6deca916389202b4bc4d
Lion~ Bernhard <Bernhard.l.Thomas@protonmail.com>
2024-03-11 14:18:38 +0100

    Add convenience functions to the BoundingBox and GeoPoint types

--- georocket_rs/georocket-types/src/lib.rs
+++ georocket_rs/georocket-types/src/lib.rs
@@ -10,6 +10,11 @@ impl GeoPoint {
     pub fn new(x: f64, y: f64) -> Self {
         Self { x, y }
     }
+    /// returns the coordinates of the `GeoPoint` as a tuple.
+    /// The values of the tuple correspond to: (x, y)
+    pub fn as_tuple(&self) -> (f64, f64) {
+        (self.x, self.y)
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone, Copy)]
@@ -17,6 +22,60 @@ pub struct BoundingBox {
     pub srid: Option<u32>,
     pub lower_left: GeoPoint,
     pub upper_right: GeoPoint,
+}
+
+impl BoundingBox {
+    /// Constructs a `BoundingBox` from two `GeoPoints and an optional srid
+    ///
+    /// # Overhead
+    /// `a` and `b` do not represent minimum or maximum points. The function will
+    /// extract the minimum and maximum values via comparison.
+    /// Construct the `BoundingBox` directly or use [`from_tuple`], if you wish to avoid
+    /// this overhead.
+    ///
+    /// ```
+    /// use georocket_types::{BoundingBox, GeoPoint};
+    /// let a = GeoPoint::new(42., 1.414);
+    /// let b = GeoPoint::new(16.07, 3.141);
+    /// let b_box = BoundingBox::new(a, b, None);
+    /// assert_eq!(
+    ///     b_box,
+    ///     BoundingBox{
+    ///         srid: None,
+    ///         lower_left: GeoPoint::new(16.07, 1.414),
+    ///         upper_right: GeoPoint::new(42., 3.141),
+    ///     });
+    /// ```
+    pub fn new(a: GeoPoint, b: GeoPoint, srid: Option<u32>) -> Self {
+        let (min_x, max_x) = if a.x < b.x { (a.x, b.x) } else { (b.x, a.x) };
+        let (min_y, may_y) = if a.y < b.y { (a.y, b.y) } else { (b.y, a.y) };
+        Self {
+            srid,
+            lower_left: GeoPoint::new(min_x, min_y),
+            upper_right: GeoPoint::new(max_x, may_y),
+        }
+    }
+    /// Constructs a `BoundingBox` from a tuple in the form of `(min_x, min_y, max_x, max_y, srid)`.
+    /// Asserts that `min_x <= max_x` and `min_y <= max_y` in debug builds.
+    pub fn from_tuple(
+        (min_x, min_y, max_x, max_y, srid): (f64, f64, f64, f64, Option<u32>),
+    ) -> Self {
+        debug_assert!(min_x <= max_x, "min_x was larger than max_x");
+        debug_assert!(min_y <= max_y, "min_y was larger than max_y");
+        Self {
+            srid: srid,
+            lower_left: GeoPoint::new(min_x, min_y),
+            upper_right: GeoPoint::new(max_x, max_y),
+        }
+    }
+    /// Returns the coordinates of the bounding box as a tuple.
+    /// The values of the tuple correspond to:
+    /// (min_x, min_y, max_x, max_y)
+    pub fn as_tuple(&self) -> (f64, f64, f64, f64) {
+        let (min_x, min_y) = self.lower_left.as_tuple();
+        let (max_x, max_y) = self.upper_right.as_tuple();
+        (min_x, min_y, max_x, max_y)
+    }
 }
 
 /// Type representing the different types allowed as values in key-value pairs.
